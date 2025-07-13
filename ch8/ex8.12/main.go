fb341156194b4b9b964648eae5cf857eb8708be2
liuxueyang <liuxueyang457@gmail.com>
2025-07-13 18:47:18 +0800

    exercise 8.12

---
+++ ch8/ex8.12/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go broadcaster()
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		go handleConn(conn)
+	}
+}
+
+type client chan<- string
+
+type Client struct {
+	name string
+	ch   chan<- string
+}
+
+var (
+	entering = make(chan Client)
+	leaving  = make(chan Client)
+	messages = make(chan string)
+)
+
+func broadcaster() {
+	clients := make(map[Client]bool)
+
+	for {
+		select {
+		case msg := <-messages:
+			for cli := range clients {
+				cli.ch <- msg
+			}
+		case cli := <-entering:
+			clients[cli] = true
+
+			for other := range clients {
+				if other != cli {
+					cli.ch <- other.name + " has joined the chat"
+				}
+			}
+		case cli := <-leaving:
+			delete(clients, cli)
+			close(cli.ch)
+		}
+	}
+}
+
+func handleConn(conn net.Conn) {
+	ch := make(chan string)
+	go clientWriter(conn, ch)
+
+	who := conn.RemoteAddr().String()
+	ch <- "You are " + who
+	messages <- who + " has arrived"
+
+	cli := Client{name: who, ch: ch}
+	entering <- cli
+
+	input := bufio.NewScanner(conn)
+	for input.Scan() {
+		messages <- who + ": " + input.Text()
+	}
+
+	leaving <- cli
+	messages <- who + " has left"
+	conn.Close()
+}
+
+func clientWriter(conn net.Conn, ch <-chan string) {
+	for msg := range ch {
+		fmt.Fprintln(conn, msg)
+	}
+}
