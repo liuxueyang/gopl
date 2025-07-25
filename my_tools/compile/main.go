31d7c4d4790554cf7d83fd3960e1112d0fd93cb7
liuxueyang <liuxueyang457@gmail.com>
2025-07-25 21:50:37 +0800

    compile command works on Windows

--- my_tools/compile/main.go
+++ my_tools/compile/main.go
@@ -1,15 +1,17 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"strings"
+	"time"
 )
 
-var cflags_arr = []string{
+var cflags = []string{
 	"-std=c++23",
 	"-Wall",
 	"-Wextra",
@@ -21,7 +23,22 @@ var cflags_arr = []string{
 	"-Wno-unused-result",
 }
 
-var use_time = flag.Bool("time", false, "Use time command to measure execution time")
+var profile = flag.Bool("profile", false, "Use time command to measure execution")
+var redirect_output = flag.Bool("redirect", false, "Redirect output to file")
+
+var islinux bool
+
+func init() {
+	islinux = isLinux()
+
+	home_path, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Println("Error getting home directory:", err)
+		return
+	}
+	debug_path := filepath.Join(home_path, "cpp", "header")
+	cflags = append(cflags, "-I", debug_path)
+}
 
 func main() {
 	flag.Parse()
@@ -44,7 +61,12 @@ func main() {
 	} else {
 		execname = basename + ".exe"
 	}
-	output := basename + ".output"
+
+	var output string
+
+	if *redirect_output {
+		output = basename + ".output"
+	}
 
 	err := compileFile(src, execname)
 	if err != nil {
@@ -60,24 +82,15 @@ func main() {
 }
 
 func compileFile(src string, execname string) error {
-	if islinux {
-		cflags_arr = append(cflags_arr, "-I/home/rakuyo/cpp/header")
-	} else {
-		// NOTE: Windows 下路径周围不要加双引号
-		cflags_arr = append(cflags_arr, "-IC:/Users/rakuy/cpp/header")
-	}
-
 	var cmd *exec.Cmd
 	srcFile := src
 
 	if islinux {
-		compileCommand := append([]string{"g++"}, cflags_arr...)
+		compileCommand := append([]string{"g++"}, cflags...)
 		compileCommand = append(compileCommand, "-o", execname, srcFile)
 		cmd = exec.Command("sh", append([]string{"-c"}, compileCommand...)...)
 	} else {
-		compileCommand := append(cflags_arr, "-o", execname, srcFile)
-
-		fmt.Printf("Compiling with args: %v\n", compileCommand)
+		compileCommand := append(cflags, "-o", execname, srcFile)
 		cmd = exec.Command("g++", compileCommand...)
 	}
 
@@ -104,51 +117,49 @@ func isLinux() bool {
 	return strings.Contains(strings.ToLower(string(uname_output)), "linux")
 }
 
-var islinux bool
-
-func init() {
-	islinux = isLinux()
+func runWithoutRedirect(execname string) error {
+	// 直接运行程序，不重定向输出
+	return runAndRedirect(execname, "")
 }
 
-func runAndRedirect(execname, outputFile string) error {
-	// 创建输出文件
-	file, err := os.Create(outputFile)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	file_err, err := os.Create(strings.TrimSuffix(outputFile, ".output") + ".err")
-	if err != nil {
-		return err
-	}
-	defer file_err.Close()
-
+func runAndRedirect(execname, outputFile string) (err error) {
 	var cmd *exec.Cmd
-	if *use_time && islinux {
-		// 如果使用 -time 标志，则使用 time 命令来测量执行时间
-		cmd = exec.Command("time", "./"+execname)
-		cmd.Stdout = file
-		cmd.Stderr = file_err // 可选：也重定向错误输出
-	} else {
-		// 执行程序并重定向输出
-		if islinux {
-			cmd = exec.Command("./" + execname)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if islinux {
+		if *profile {
+			// 如果使用 -profile 标志，则使用 time 命令来测量执行时间
+			cmd = exec.CommandContext(ctx, "time", "./"+execname)
 		} else {
-			cmd = exec.Command(".\\" + execname)
+			cmd = exec.CommandContext(ctx, "./"+execname)
 		}
+	} else {
+		// TODO: time is not supported on Windows
+		cmd = exec.CommandContext(ctx, ".\\"+execname)
+	}
+
+	if len(outputFile) > 0 {
+		// 创建输出文件
+		file, err := os.Create(outputFile)
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+
+		file_err, err := os.Create(strings.TrimSuffix(outputFile, ".output") + ".err")
+		if err != nil {
+			return err
+		}
+		defer file_err.Close()
+
 		cmd.Stdout = file
-		cmd.Stderr = file_err // 可选：也重定向错误输出
+		cmd.Stderr = file_err
+	} else {
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
 	}
 
 	return cmd.Run()
 }
-
-// func _debugShowWorkingDir() {
-// 	cwd, err := os.Getwd()
-// 	if err != nil {
-// 		fmt.Println("Error getting current working directory:", err)
-// 		return
-// 	}
-// 	fmt.Println("Current working directory:", cwd)
-// }
